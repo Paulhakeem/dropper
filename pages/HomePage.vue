30350e5e5ba73a3e7affaf257b9987c742d9c592
Paulhakeem <paulnyamawi18@gmail.com>
2024-02-11 18:57:03 +0300

    adding some styling

--- pages/HomePage.vue
+++ pages/HomePage.vue
@@ -4,7 +4,7 @@
     <sidebar />
     <!-- end of sidebar -->
 
-    <div id="home" class="p-4 sm:ml-64 md:pt-24 lg:pt-32">
+    <div id="home" class="p-4 sm:ml-64 md:pt-18">
       <div class="p-4 h-96">
         <div id="homepage" class="text-center justify-center m-auto">
           <h2 class="text-text-color font-semibold text-2xl md:text-4xl">Welcome!!🤗</h2>
@@ -12,7 +12,7 @@
             Get started by uploading your files
           </p>
         </div>
-        <div id="input-page" class="bg-light-gray rounded-md mt-14 mb-44">
+        <div id="input-page" class="bg-light-gray rounded-md mt-14">
           <div class="flex gap-6 justify-between">
             <button
               class="text-text-color uppercase font-medium p-4 tracking-widest w-full"
